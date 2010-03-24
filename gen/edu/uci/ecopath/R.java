2e9e7fd1fb14477834af96a2300fac667efbd607
highbulp <highbulp@gmail.com>
2010-03-24 14:49:40 -0700

    Attempted inclusion of mapping into stuff. Still broken

--- gen/edu/uci/ecopath/R.java
+++ gen/edu/uci/ecopath/R.java
@@ -14,12 +14,13 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int data=0x7f050005;
-        public static final int go_button=0x7f050003;
-        public static final int latitude=0x7f050001;
-        public static final int longitude=0x7f050002;
-        public static final int status=0x7f050004;
-        public static final int text=0x7f050000;
+        public static final int data=0x7f050006;
+        public static final int go_button=0x7f050004;
+        public static final int latitude=0x7f050002;
+        public static final int longitude=0x7f050003;
+        public static final int mapview=0x7f050000;
+        public static final int status=0x7f050005;
+        public static final int text=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/edu/uci/ecopath/EcoPath.java
+++ src/edu/uci/ecopath/EcoPath.java
@@ -6,7 +6,10 @@ import java.util.Map;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.util.EntityUtils;
-import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,13 +17,26 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
 
-public class EcoPath extends Activity
+public class EcoPath extends MapActivity
 {
 	private static final String TAG = "ECOPATH";
-	TextView sv;
-	TextView dv;
-	EditText lat, lng;
+	private TextView sv;
+	private TextView dv;
+	private EditText lat, lng;
+
+	private LocationManager lm;
+	private MapController mc;
+	private MapView mapView;
+	
+	double curLat = 33.642937;
+	double curLng = -117.841411;
+
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -29,6 +45,13 @@ public class EcoPath extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		Log.v(TAG,"View set, logging working");
+
+		lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);    
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyLocationListener());
+
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		mc = mapView.getController();
 		
 		Button goButton = (Button) findViewById(R.id.go_button);
 		goButton.setOnClickListener(goButtonListener);
@@ -63,6 +86,32 @@ public class EcoPath extends Activity
 			dv.setText(response);
 		}		
 	};
+
+	//from: http://www.devx.com/wireless/Article/39239/1954
+	private class MyLocationListener implements LocationListener 
+	{
+		public void onLocationChanged(Location loc) {
+			if (loc != null) {
+				curLat = loc.getLatitude();
+				curLng = loc.getLongitude();
+				Toast.makeText(getBaseContext(), 
+						"Location changed: Lat: " + loc.getLatitude() + " Lng: " + loc.getLongitude(), 
+						Toast.LENGTH_SHORT).show();
+				Log.v(TAG, "Latitude: "+curLat+" Longitude: "+curLng);
+				GeoPoint p = new GeoPoint((int) (loc.getLatitude() * 1E6), (int) (loc.getLongitude() * 1E6));
+				mc.animateTo(p);
+				mc.setZoom(16);                
+				mapView.invalidate();
+			}
+		}
+
+		public void onProviderDisabled(String provider) {}
+		public void onProviderEnabled(String provider) {}
+		public void onStatusChanged(String provider, int status, Bundle extras) {}
+	}
+
+	protected boolean isRouteDisplayed()
+	{ return false; }
 
 	
 }
